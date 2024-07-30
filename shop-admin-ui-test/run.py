6f2c16c85b0d3a5deda0a017eeda8a56a4117c19
zhouhuajian <zhouhuajian@company.com>
2024-07-30 18:37:09 +0800

    06 自动测试订单设置

--- shop-admin-ui-test/run.py
+++ shop-admin-ui-test/run.py
@@ -3,4 +3,5 @@ import pytest
 if __name__ == '__main__':
     # pytest.main()
     # pytest.main(["./tests/test_login_page.py", "./tests/test_add_product_page.py"])
-    pytest.main(["./tests/test_login_page.py", "./tests/test_pms_brand_page.py"])
+    # pytest.main(["./tests/test_login_page.py", "./tests/test_pms_brand_page.py"])
+    pytest.main(["./tests/test_login_page.py", "./tests/test_oms_order_setting_page.py"])
---
+++ shop-admin-ui-test/tests/test_oms_order_setting_page.py
@@ -0,0 +1,54 @@
+from selenium.webdriver import ActionChains, Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.wait import WebDriverWait
+
+
+def test_order_setting_form_validation(baseurl, driver):
+    driver.get(baseurl + "/oms/order-setting")
+    # 1
+    element = driver.find_element(By.CSS_SELECTOR, ".el-form-item:nth-child(1) .el-input__inner")
+    actions = ActionChains(driver)
+    actions.double_click(element).perform()
+    driver.find_element(By.CSS_SELECTOR, ".el-form-item:nth-child(1) .el-input__inner").send_keys(Keys.BACKSPACE)
+    driver.find_element(By.CSS_SELECTOR, "html").click()
+    WebDriverWait(driver, 5).until(expected_conditions.text_to_be_present_in_element(
+        (By.CSS_SELECTOR, ".el-form-item:nth-child(1) .el-form-item__error"), "请输入大于0的正整数"))
+    # 2
+    element = driver.find_element(By.CSS_SELECTOR, ".el-form-item:nth-child(2) .el-input__inner")
+    actions = ActionChains(driver)
+    actions.double_click(element).perform()
+    driver.find_element(By.CSS_SELECTOR, ".el-form-item:nth-child(2) .el-input__inner").send_keys("aaa")
+    driver.find_element(By.CSS_SELECTOR, "html").click()
+    WebDriverWait(driver, 5).until(expected_conditions.text_to_be_present_in_element(
+        (By.CSS_SELECTOR, ".el-form-item:nth-child(2) .el-form-item__error"), "请输入大于0的正整数"))
+    # 3
+    element = driver.find_element(By.CSS_SELECTOR, ".el-form-item:nth-child(3) .el-input__inner")
+    actions = ActionChains(driver)
+    actions.double_click(element).perform()
+    driver.find_element(By.CSS_SELECTOR, ".el-form-item:nth-child(3) .el-input__inner").send_keys("15.1")
+    driver.find_element(By.CSS_SELECTOR, "html").click()
+    WebDriverWait(driver, 5).until(expected_conditions.text_to_be_present_in_element(
+        (By.CSS_SELECTOR, ".el-form-item:nth-child(3) .el-form-item__error"), "请输入大于0的正整数"))
+    # 4
+    element = driver.find_element(By.CSS_SELECTOR, ".el-form-item:nth-child(4) .el-input__inner")
+    actions = ActionChains(driver)
+    actions.double_click(element).perform()
+    driver.find_element(By.CSS_SELECTOR, ".el-form-item:nth-child(4) .el-input__inner").send_keys("-7")
+    driver.find_element(By.CSS_SELECTOR, "html").click()
+    WebDriverWait(driver, 5).until(expected_conditions.text_to_be_present_in_element(
+        (By.CSS_SELECTOR, ".el-form-item:nth-child(4) .el-form-item__error"), "请输入大于0的正整数"))
+    # 5
+    element = driver.find_element(By.CSS_SELECTOR, ".el-form-item:nth-child(5) .el-input__inner")
+    actions = ActionChains(driver)
+    actions.double_click(element).perform()
+    driver.find_element(By.CSS_SELECTOR, ".el-form-item:nth-child(5) .el-input__inner").send_keys("0")
+    driver.find_element(By.CSS_SELECTOR, "html").click()
+    WebDriverWait(driver, 5).until(expected_conditions.text_to_be_present_in_element(
+        (By.CSS_SELECTOR, ".el-form-item:nth-child(5) .el-form-item__error"), "请输入大于0的正整数"))
+    # 提交
+    driver.find_element(By.CSS_SELECTOR, ".el-button").click()
+    WebDriverWait(driver, 5).until(
+        expected_conditions.text_to_be_present_in_element((By.CSS_SELECTOR, ".el-message__content"),
+                                                          "请填写正确后再提交"))
+
